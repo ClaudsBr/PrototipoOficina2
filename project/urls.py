c891bb4340a6a3b5eabcccbd3e300764564beb26
coot <claudio-oliveira.santos@gft.com>
2022-06-23 18:17:35 -0300

    Adicionando funcionalidades

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import home, create, store, painel, logar, dashboard, deslogar, administrador, usuario, contato, endereco, veiculo, criarCliente, criarEndereco, criarVeiculo, view, editCliente, editEndereco, editVeiculo, updateCliente, updateEndereco, updateVeiculo, deleteCliente
+from app.views import home, create, store, painel, logar, dashboard, deslogar, administrador, usuario, contato, endereco, veiculo, criarCliente, criarEndereco, criarVeiculo, view, editCliente, editEndereco, editVeiculo, updateCliente, updateEndereco, updateVeiculo, deleteCliente, senha, changePassword
 
 
 urlpatterns = [
@@ -44,7 +44,9 @@ urlpatterns = [
     path('updateEndereco/<int:pk>/', updateEndereco, name='updateEndereco'),
     path('updateVeiculo/<int:pk>/', updateVeiculo, name='updateVeiculo'),
     path('usuario/deleteCliente/<int:pk>/', deleteCliente, name='deleteCliente'),
-
+    path('senha/', senha),
+    path('changePassword/', changePassword),
+    path('administrador/', administrador)
 
 
 ]
